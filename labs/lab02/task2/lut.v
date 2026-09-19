e1b47311e346aa53d24d0eaf02976ae7111bf324
Manan Gupta <104864714+mgisabsolute@users.noreply.github.com>
2026-09-19 15:58:47 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -2,7 +2,7 @@
 // A small parameterized ROM (lookup table): DEPTH words, each WIDTH bits
 // wide. dout continuously reflects mem[sel].
 //
-// YOU complete the two TODOs below. Everything else is given.
+// Contents are initialized once; reads are combinational.
 
 module lut #(
   parameter WIDTH = 8,
@@ -16,13 +16,13 @@ module lut #(
 
   integer i;
 
-  // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
-  // Use an initial block with a for loop -- this is the only place a ROM's
-  // contents should be set up. (See the lab manual for why.)
-  
+  initial begin
+    for (i = 0; i < DEPTH; i = i + 1)
+      mem[i] = i * i;
+  end
 
-  // TODO: make dout continuously reflect mem[sel]. This is a combinational
-  // read -- pick the right procedural block and sensitivity list.
-
+  always @(*) begin
+    dout = mem[sel];
+  end
 
 endmodule
